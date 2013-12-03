8ba8376adc10a5e865f44c8981ec034e6803572c
Hiroto Takahashi <guild0105@gmail.com>
2013-12-03 15:37:57 +0900

    initliaze commit

---
+++ src/RCConfig.java
@@ -0,0 +1,20 @@
+
+public class RCConfig {
+
+	//------------------- system -------------------//
+	public static String title_frame = "RollCakeRSS";
+
+
+
+
+	/* --------------------------------------------------------- *
+	 *     main value setting
+	 * --------------------------------------------------------- */
+	public static int num_day_recentry = 3;
+
+
+	//------------------- grid layout -------------------//
+	public static int num_split_column_hour = 4;
+
+
+}
